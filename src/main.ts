0cc775817221c7e8ef79d07d546f0ce918f067ab
Tri Nguyen <nguyendinhtri032@gmail.com>
2024-12-08 11:27:39 +0700

    add docs api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('Cardhub API')
+    .setDescription('Open up the world')
+    .setVersion('1.0')
+    .addTag('Cardhub')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
   await app.listen(3000);
 }
 bootstrap();
